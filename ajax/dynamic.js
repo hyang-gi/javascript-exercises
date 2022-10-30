59b86848e21bf3d119be731cf52aa04ccd071965
Nikhat Mohsin <nikhatmohsin22@gmail.com>
2022-10-30 10:20:58 +0000

    display data with specific values only

--- ajax/dynamic.js
+++ ajax/dynamic.js
@@ -3,17 +3,24 @@ $(document).ready(function() {
     $.ajax({
         url: "mock_data.json",
         success: function(data) {
-            console.log(data);
+            //console.log(data);
             renderDataInTheTable(data);
         }
     });
 
     function renderDataInTheTable(data) {
         const mytable = document.getElementById("html-data-table");
-        data.forEach(row => {
-            let newRow = document.createElement("tr");
-            Object.values(row).forEach((value) => {
-                let cell = document.createElement("td");
+        var newData = data.map(function (item) {
+            return {
+                name: `${item.first_name} ${item.last_name}`,
+                high_score: item.high_score,
+            }
+        });
+        newData.map(row => {
+            let newRow = document.createElement("tr"); // new row is created
+            Object.values(row).map((value) => {
+                //console.log(value);
+                let cell = document.createElement("td"); // new data for the row is added
                 cell.innerText = value;
                 newRow.appendChild(cell);
             })
